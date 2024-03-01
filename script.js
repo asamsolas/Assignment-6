455bd439589154fa97efc18f509970882cee2464
asamsolas <157108453+asamsolas@users.noreply.github.com>
2024-03-01 01:34:02 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function addTask() {
+    const taskInput = document.getElementById('taskInput');
+    const taskList = document.getElementById('taskList');
+
+    if (taskInput.value.trim() !== '') {
+        const li = document.createElement('li');
+        li.innerHTML = `
+            <span>${taskInput.value}</span>
+            <button onclick="removeTask(this)">Remove</button>
+        `;
+        taskList.appendChild(li);
+        taskInput.value = '';
+    }  
+}
+
+function removeTask(button) {
+    const li = button.parentNode;
+    li.parentNode.removeChild(li);  
+};
